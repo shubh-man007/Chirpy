fb1e6a173c43af492b7aeb7c6f3cd559db6bc726
shubh-man007 <randombro2077@gmail.com>
2025-11-25 15:24:42 +0530

    Refactor

--- main.go
+++ main.go
@@ -1,39 +1,3 @@
-// package main
-
-// import (
-// 	"fmt"
-// 	"time"
-// )
-
-// type Request struct {
-// 	Path string
-// }
-
-// func handleRequest(req Request) {
-// 	fmt.Printf("Processing %s\n", req.Path)
-// 	time.Sleep(2 * time.Second)
-// 	fmt.Printf("Processed %s\n", req.Path)
-// }
-
-// func handleRequests(reqs <-chan Request) {
-// 	for req := range reqs {
-// 		fmt.Printf(">> %s\n", req)
-// 		go handleRequest(req)
-// 	}
-// }
-
-// func main() {
-// 	reqs := make(chan Request, 100)
-// 	go handleRequests(reqs)
-// 	for i := 0; i < 10; i++ {
-// 		reqs <- Request{Path: fmt.Sprintf("/path/%d", i)}
-// 		time.Sleep(500 * time.Millisecond)
-// 	}
-
-// 	time.Sleep(5 * time.Second)
-// 	fmt.Println("5 seconds passed, killing server")
-// }
-
 package main
 
 import (
@@ -41,32 +5,31 @@ import (
 	"time"
 )
 
-func handleRequests(reqs <-chan request) {
+type Request struct {
+	Path string
+}
+
+func handleRequest(req Request) {
+	fmt.Printf("Processing %s\n", req.Path)
+	time.Sleep(2 * time.Second)
+	fmt.Printf("Processed %s\n", req.Path)
+}
+
+func handleRequests(reqs <-chan Request) {
 	for req := range reqs {
+		fmt.Printf(">> %s\n", req)
 		go handleRequest(req)
 	}
 }
 
-// don't touch below this line
-
-type request struct {
-	path string
-}
-
 func main() {
-	reqs := make(chan request, 100)
+	reqs := make(chan Request, 100)
 	go handleRequests(reqs)
 	for i := 0; i < 10; i++ {
-		reqs <- request{path: fmt.Sprintf("/path/%d", i)}
+		reqs <- Request{Path: fmt.Sprintf("/path/%d", i)}
 		time.Sleep(500 * time.Millisecond)
 	}
 
 	time.Sleep(5 * time.Second)
 	fmt.Println("5 seconds passed, killing server")
-}
-
-func handleRequest(req request) {
-	fmt.Println("Handling request for", req.path)
-	time.Sleep(2 * time.Second)
-	fmt.Println("Done with request for", req.path)
 }
