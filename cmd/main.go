815d9aebe0879ec51804ff0405f099b84038b3e4
shubh-man007 <randombro2077@gmail.com>
2025-11-26 19:07:12 +0530

    Middleware addition

--- cmd/main.go
+++ cmd/main.go
@@ -1,34 +1,61 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	rootHits atomic.Int32
+}
+
+func (cfg *apiConfig) HitCounterMiddleware(next http.Handler) http.Handler {
+	newHit := cfg.rootHits.Add(1)
+	cfg.rootHits.Store(newHit)
+	return next
+}
 
 func main() {
 	const port = "8080"
 
+	cfg := &apiConfig{}
 	mux := http.NewServeMux()
 
-	// mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	// 	greetings, err := os.ReadFile("assets/index.html")
-	// 	if err != nil {
-	// 		log.Println("could not find file to serve")
-	// 	}
-	// 	w.Write(greetings)
-	// })
-
 	statDir := http.Dir("./static")
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(statDir)))
+	mux.Handle("/app/", http.StripPrefix("/app/", cfg.HitCounterMiddleware(http.FileServer(statDir))))
 
 	assetsDir := http.Dir("./assets")
 	mux.Handle("/app/assets/", http.StripPrefix("/app/assets/", http.FileServer(assetsDir)))
 
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodGet {
-			w.WriteHeader(200)
+			w.WriteHeader(http.StatusOK)
 			w.Header().Set("Content-Type", "text/plain")
 			w.Write([]byte("OK , We are Chirping"))
+		}
+	})
+
+	mux.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			hits := cfg.rootHits.Load()
+			strHits := fmt.Sprintf("Hits: %s", strconv.Itoa(int(hits)))
+			w.WriteHeader(http.StatusOK)
+			w.Header().Set("Content-Type", "text/plain")
+			w.Write([]byte(strHits))
+		}
+	})
+
+	mux.HandleFunc("/reset", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			cfg.rootHits.Store(0)
+			hits := cfg.rootHits.Load()
+			strHits := fmt.Sprintf("Hits: %s", strconv.Itoa(int(hits)))
+			w.WriteHeader(http.StatusOK)
+			w.Header().Set("Content-Type", "text/plain")
+			w.Write([]byte(strHits))
 		}
 	})
 
