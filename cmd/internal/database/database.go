a98f697b5692fa99b8aa5e33f0b015b7975df898
shubh-man007 <randombro2077@gmail.com>
2026-01-08 22:45:50 +0530

    DB setup

---
+++ cmd/internal/database/database.go
@@ -0,0 +1,16 @@
+package database
+
+// import (
+// 	"database/sql"
+// 	"os"
+
+// 	"github.com/joho/godotenv"
+// 	_ "github.com/lib/pq"
+// )
+
+// func main() {
+// 	godotenv.Load()
+
+// 	dbURL := os.Getenv("DB_URL")
+// 	db, err := sql.Open("postgres", dbURL)
+// }
