d04b34f4a13d4ff60bc42d42fcb21c350728a2e8
shubh-man007 <randombro2077@gmail.com>
2026-01-11 02:11:30 +0530

    DB connection with handlers

--- cmd/chirpy/main.go
+++ cmd/chirpy/main.go
@@ -3,13 +3,22 @@ package main
 import (
 	"log"
 
+	"github.com/shubh-man007/Chirpy/cmd/internal/database"
 	"github.com/shubh-man007/Chirpy/cmd/internal/server"
 )
 
 func main() {
 	const port = "8080"
 
-	srv := server.New(port, nil)
+	pgx, err := database.NewDbPgx()
+	if err != nil {
+		log.Fatalf("failed connecting to DB: %v", err)
+	}
+	defer pgx.Close()
+
+	log.Print("connected to DB")
+
+	srv := server.New(port, pgx.Queries)
 	if err := srv.Start(); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
--- cmd/internal/database/database.go
+++ cmd/internal/database/database.go
@@ -1,16 +1,44 @@
 package database
 
-// import (
-// 	"database/sql"
-// 	"os"
+import (
+	"database/sql"
+	"log"
+	"os"
 
-// 	"github.com/joho/godotenv"
-// 	_ "github.com/lib/pq"
-// )
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
 
-// func main() {
-// 	godotenv.Load()
+type DbPgx struct {
+	DB      *sql.DB
+	Queries *Queries
+}
 
-// 	dbURL := os.Getenv("DB_URL")
-// 	db, err := sql.Open("postgres", dbURL)
-// }
+func NewDbPgx() (*DbPgx, error) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Printf("could not load .env file: %s", err)
+		return nil, err
+	}
+
+	connStr := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	queries := New(db)
+
+	return &DbPgx{
+		DB:      db,
+		Queries: queries,
+	}, nil
+}
+
+func (d *DbPgx) Close() error {
+	return d.DB.Close()
+}
