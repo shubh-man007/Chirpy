86411646b5ea7a90ecc7dec272ac477c5429eaee
shubh-man007 <randombro2077@gmail.com>
2026-01-11 00:01:53 +0530

    db comment fix

--- cmd/chirpy/main.go
+++ cmd/chirpy/main.go
@@ -9,14 +9,6 @@ import (
 func main() {
 	const port = "8080"
 
-	// TODO: Initialize database connection when ready
-	// db, err := database.OpenDB(...)
-	// if err != nil {
-	// 	log.Fatalf("Failed to open database: %v", err)
-	// }
-	// defer db.Close()
-	// queries := database.New(db)
-
 	srv := server.New(port, nil)
 	if err := srv.Start(); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
