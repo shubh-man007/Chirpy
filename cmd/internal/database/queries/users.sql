c2969086768c276f94fac12b2f8b117067c0167d
shubh-man007 <randombro2077@gmail.com>
2026-01-20 23:24:53 +0530

    Update user table and queries to account for and update membership

--- cmd/internal/database/queries/users.sql
+++ cmd/internal/database/queries/users.sql
@@ -7,13 +7,13 @@ VALUES (
     $1,
     $2
 )
-RETURNING id, created_at, updated_at, email;
+RETURNING id, created_at, updated_at, email, is_chirpy_red;
 
 -- name: GetUserPassByEmail :one
 SELECT hashed_password FROM users WHERE email = $1;
 
 -- name: GetUserByEmail :one
-SELECT id, created_at, updated_at, email FROM users WHERE email = $1;
+SELECT id, created_at, updated_at, email, is_chirpy_red FROM users WHERE email = $1;
 
 -- name: DeleteUser :exec
 DELETE FROM users;
@@ -24,4 +24,10 @@ SET updated_at = NOW(),
     email = $1,
     hashed_password = $2
 WHERE id = $3
-RETURNING id, created_at, updated_at, email;
+RETURNING id, created_at, updated_at, email, is_chirpy_red;
+
+-- name: UpdateUserToChirpyRed :exec
+UPDATE users
+SET is_chirpy_red = TRUE,
+    updated_at = NOW()
+WHERE id = $1;
