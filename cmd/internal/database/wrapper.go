d5417b073ba1c227e34357f7e07b2c5dcf7518ae
shubh-man007 <randombro2077@gmail.com>
2026-02-10 00:16:50 +0530

    Wrapper for rejection

--- cmd/internal/database/wrapper.go
+++ cmd/internal/database/wrapper.go
@@ -5,11 +5,34 @@ import (
 	"database/sql"
 )
 
-func (q *Queries) AcceptFriendRequestSafely(
-	ctx context.Context,
-	arg AcceptFriendRequestParams,
-) error {
+func (q *Queries) AcceptFriendRequestSafely(ctx context.Context, arg AcceptFriendRequestParams) error {
 	res, err := q.db.ExecContext(ctx, acceptFriendRequest, arg.UserID, arg.FriendID)
+	if err != nil {
+		return err
+	}
+
+	rows, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rows == 0 {
+		return sql.ErrNoRows
+	}
+
+	return nil
+}
+
+func (q *Queries) RejectFriendRequestSafely(
+	ctx context.Context,
+	arg RejectFriendRequestParams,
+) error {
+	res, err := q.db.ExecContext(
+		ctx,
+		rejectFriendRequest,
+		arg.UserID,
+		arg.FriendID,
+	)
 	if err != nil {
 		return err
 	}
