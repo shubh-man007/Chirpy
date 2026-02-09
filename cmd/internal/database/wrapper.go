1ff3f319a5d46cd35a99e9ff2bd6f887d767e42d
shubh-man007 <randombro2077@gmail.com>
2026-02-09 23:45:57 +0530

    Check affected rows while accepting friend requests

---
+++ cmd/internal/database/wrapper.go
@@ -0,0 +1,27 @@
+package database
+
+import (
+	"context"
+	"database/sql"
+)
+
+func (q *Queries) AcceptFriendRequestSafely(
+	ctx context.Context,
+	arg AcceptFriendRequestParams,
+) error {
+	res, err := q.db.ExecContext(ctx, acceptFriendRequest, arg.UserID, arg.FriendID)
+	if err != nil {
+		return err
+	}
+
+	rows, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rows == 0 {
+		return sql.ErrNoRows
+	}
+
+	return nil
+}
