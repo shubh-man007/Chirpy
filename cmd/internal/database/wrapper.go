027e921a7da16b374093e921e29a23f7337b3bb0
shubh-man007 <randombro2077@gmail.com>
2026-02-13 00:35:03 +0530

    Commented code

--- cmd/internal/database/wrapper.go
+++ cmd/internal/database/wrapper.go
@@ -1,1 +1,50 @@
 package database
+
+// import (
+// 	"context"
+// 	"database/sql"
+// )
+
+// func (q *Queries) AcceptFriendRequestSafely(ctx context.Context, arg AcceptFriendRequestParams) error {
+// 	res, err := q.db.ExecContext(ctx, acceptFriendRequest, arg.UserID, arg.FriendID)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	rows, err := res.RowsAffected()
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	if rows == 0 {
+// 		return sql.ErrNoRows
+// 	}
+
+// 	return nil
+// }
+
+// func (q *Queries) RejectFriendRequestSafely(
+// 	ctx context.Context,
+// 	arg RejectFriendRequestParams,
+// ) error {
+// 	res, err := q.db.ExecContext(
+// 		ctx,
+// 		rejectFriendRequest,
+// 		arg.UserID,
+// 		arg.FriendID,
+// 	)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	rows, err := res.RowsAffected()
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	if rows == 0 {
+// 		return sql.ErrNoRows
+// 	}
+
+// 	return nil
+// }
