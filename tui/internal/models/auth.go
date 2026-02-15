2cbd3a5b73be5a2c4e9055867ab0ef892c8a68bd
shubh-man007 <randombro2077@gmail.com>
2026-02-15 23:52:34 +0530

    Updating auth model

--- tui/internal/models/auth.go
+++ tui/internal/models/auth.go
@@ -1,5 +1,9 @@
 package models
 
+type LoginCreds struct {
+	Password string `json:"password"`
+	Email    string `json:"email"`
+}
 type LoginResponse struct {
 	UserID       string `json:"user_id"`
 	IsChirpyRed  bool   `json:"is_chirpy_red"`
