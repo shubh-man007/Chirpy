0fe030d96b22675f6e065d23c2cf736ae224c807
shubh-man007 <randombro2077@gmail.com>
2026-02-17 03:39:52 +0530

    Signals for TUI event loop

---
+++ tui/internal/ui/messages.go
@@ -0,0 +1,24 @@
+package ui
+
+import "github.com/shubh-man007/Chirpy/tui/internal/models"
+
+type LoginSuccessMsg struct {
+	User *models.User
+}
+
+type LoginFailureMsg struct {
+	Err error
+}
+
+type ErrorMsg struct {
+	Err error
+}
+
+type FeedLoadedMsg struct {
+	Chirps []models.Chirp
+	Append bool
+}
+
+type ChirpPostedMsg struct {
+	Chirp *models.Chirp
+}
