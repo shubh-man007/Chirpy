8cdc8f46c0fa706ca896c576c8b884c2f949947f
shubh-man007 <randombro2077@gmail.com>
2026-02-24 23:53:21 +0530

    Response model for finding followers via userID

--- tui/internal/models/follows.go
+++ tui/internal/models/follows.go
@@ -5,10 +5,39 @@ import "time"
 type Follow struct {
 	FolloweeID string `json:"followee_id"`
 }
+
 type FollowResponse struct {
 	Followers []FollowUser `json:"followers,omitempty"`
 	Following []FollowUser `json:"following,omitempty"`
 	Total     int64        `json:"total"`
+}
+
+type FollowersResponse struct {
+	Followers     []FollowerRow `json:"Followers"`
+	FollowerCount int64         `json:"FollowerCount"`
+}
+
+type FollowingResponse struct {
+	Following      []FollowingRow `json:"Following"`
+	FollowingCount int64          `json:"FollowingCount"`
+}
+
+type FollowerRow struct {
+	ID             string    `json:"id"`
+	Email          string    `json:"email"`
+	IsChirpyRed    bool      `json:"is_chirpy_red"`
+	CreatedAt      time.Time `json:"created_at"`
+	FollowedAt     time.Time `json:"followed_at"`
+	TotalFollowers int64     `json:"total_followers"`
+}
+
+type FollowingRow struct {
+	ID             string    `json:"id"`
+	Email          string    `json:"email"`
+	IsChirpyRed    bool      `json:"is_chirpy_red"`
+	CreatedAt      time.Time `json:"created_at"`
+	FollowedAt     time.Time `json:"followed_at"`
+	TotalFollowing int64     `json:"total_following"`
 }
 
 type FollowUser struct {
